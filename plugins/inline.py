b4155740ad834138f9216e645080dcee989f5e2a
SUPEREL <108293740+SUPEREL@users.noreply.github.com>
2022-07-07 15:59:23 +0100

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -33,7 +33,7 @@ async def answer(bot, query):
     if AUTH_CHANNEL and not await is_subscribed(bot, query):
         await query.answer(results=[],
                            cache_time=0,
-                           switch_pm_text='You have to subscribe my channel to use the bot',
+                           switch_pm_text='Vous devez vous abonner à ma chaîne pour utiliser le bot',
                            switch_pm_parameter="subscribe")
         return
 
@@ -70,11 +70,11 @@ async def answer(bot, query):
                 title=file.file_name,
                 file_id=file.file_id,
                 caption=f_caption,
-                description=f'Size: {get_size(file.file_size)}\nType: {file.file_type}',
+                description=f'Taille: {get_size(file.file_size)}\nType: {file.file_type}',
                 reply_markup=reply_markup))
 
     if results:
-        switch_pm_text = f"{emoji.FILE_FOLDER} Results - {total}"
+        switch_pm_text = f"{emoji.FILE_FOLDER} Résultats - {total}"
         if string:
             switch_pm_text += f" for {string}"
         try:
@@ -89,7 +89,7 @@ async def answer(bot, query):
         except Exception as e:
             logging.exception(str(e))
     else:
-        switch_pm_text = f'{emoji.CROSS_MARK} No results'
+        switch_pm_text = f'{emoji.CROSS_MARK} Aucun résultat'
         if string:
             switch_pm_text += f' for "{string}"'
 
